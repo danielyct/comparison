cf240baab22edd2371c99973fbf76be24e50a53e
danielyct <yudaniel40@gmail.com>
2022-04-05 20:32:24 +0800

    minor changes

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import norm
-import seaborn as sns
 # %matplotlib inline
 
-def mc(data):
-    data.reindex(index=data.index[::-1])
+def mc(data, predict_days, trials = 1000):
+    print(data)
+    print(data.size)
     pct_change = []
     for i in range(data.size):
         if i == data.size - 1:
             break
-        new = data.iloc[i]
-        old = data.iloc[i+1]
+        new = data.iloc[i + 1]
+        old = data.iloc[i]
         pct_change.append((new - old) / old)
 
     pct_change = pd.DataFrame(pct_change)
@@ -20,27 +20,24 @@ def mc(data):
 
     u = log_returns.mean()
     var = log_returns.var()
-
     drift = u - (0.5*var)
     stddev = log_returns.std()
 
-    days = 200
-    trials = 10000
-
+    days = predict_days + 1
     Z = norm.ppf(np.random.rand(days,trials)) # Percent point function
-
     daily_returns = np.exp(drift.values + stddev.values * Z)
-    daily_returns
-   
+
     S0 = data.iloc[-1]
+    print(S0)
     price_list = np.zeros_like(daily_returns)
     price_list[0] = S0
     avg_prediction = []
 
     for t in range(1,days):
        price_list[t] = price_list[t-1]*daily_returns[t]
+       avg_prediction.append(price_list[t].mean())
 
-    return price_list
+    return price_list, avg_prediction
 
 def reg(data):
     
@@ -53,41 +50,48 @@ def reg(data):
 
 def mse(prediction, real):
     if len(prediction) != len(real):
-        return undefined
-    
+        return -1
     length = len(prediction)
     mse = 0
-
-    for i in length:
-        error = prediction,iloc[i] - real.iloc[i]
+    for i in range(length):
+        error = prediction[i] - real[i]
         mse = mse + (error**2)
 
     mse = mse / length
 
     return mse
-        
+
 raw = pd.read_csv("AMD.csv")
-# raw[:10]
-raw.set_index(pd.DatetimeIndex(raw['Date']), inplace=True)
+raw = raw[::-1].reset_index(drop = True)    # Reset index, start with the oldest record
 data = raw.loc[:," Close"]
 data.columns = ["Close"]
-data = pd.DataFrame(data)
+total_days = data.size
+predict_days = 200
+real_data = data.iloc[-predict_days:].reset_index(drop = True)  # extract the last 200 days for the comparison
+training_data = data.iloc[0:total_days - predict_days].reset_index(drop = True) 
+training_data = pd.DataFrame(training_data)
 
-mc_price = mc(data)
-# reg_price = reg(data)
+trials = 1000
+mc_model, mc_price = mc(training_data, predict_days, trials)
+# reg_price = reg(training_data)
 
-mc_mse = mse(mc_price, data)
-# reg_mse = mse(reg_price, data)
-
-    
-
+mc_mse = mse(mc_price, real_data)
+# reg_mse = mse(reg_price, real_data)
 
 fig, ax = plt.subplots()
 ax.set_title("prediction over 200 days")
 ax.set_xlabel("date")
 ax.set_ylabel("price")
-ax.plot(data, label = "Real stock price")
+ax.plot(real_data, label = "Real stock price")
 ax.plot(mc_price, label = "Monte Carlo")
 # ax.plot(reg_price, label = "Linear Regression")
+ax.legend()
+
+fig2, ax2 = plt.subplots()
+ax2.plot(mc_model)
+print("mse of monte carlo:")
+print(mc_mse)
+# print("mse of linear regression:")
+# print(reg_mse)
 
 plt.show()
