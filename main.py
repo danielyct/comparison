5fa07b52e79e3dc7195105b06da7fa47c92f73f7
danielyct <yudaniel40@gmail.com>
2022-04-05 17:46:34 +0800

    very close

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import seaborn as sns
 import monte_carlo as mc
-import regression as reg
+# import regression as reg
 # %matplotlib inline
 
 raw = pd.DataFrame()
@@ -15,5 +15,13 @@ data = raw.loc[:," Close"]
 data.columns = ["Close"]
 data = pd.DataFrame(data)
 
-mc.main(data)
-reg.reg(data)
+mc_price = mc.mc(data)
+# reg_price, reg_mse = reg.reg(data)
+
+fig, ax = plt.subplots()
+ax.set_title("prediction over 200 days")
+ax.set_xlabel("date")
+ax.set_ylabel("price")
+ax.plot(data, label = "Real stock price")
+ax.plot(mc_price, label = "Monte Carlo")
+# ax.plot(reg_price, label = "Linear Regression")
